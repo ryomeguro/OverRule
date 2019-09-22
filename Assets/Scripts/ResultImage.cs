ce2598258768ba6c0642f74304717cef36d5e9b8
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-22 20:54:43 +0900

    帰宅

---
+++ Assets/Scripts/ResultImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultImage : MonoBehaviour
+{
+    private Image image;
+    public Sprite[] Sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = this.GetComponent<Image>();
+        
+        if(GameInfo.winNum[0]< GameInfo.winNum[1])
+        {
+            image.sprite = Sprite[0];
+        }
+        else
+        {
+            image.sprite = Sprite[1];
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -2,30 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ResultManager : MonoBehaviour
 {
-    public Text result;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        try
-        {
-            result.text += "player1: ";
-            result.text += GameInfo.winNum[0].ToString();
-            result.text += "-";
-            result.text += GameInfo.winNum[1].ToString();
-            result.text += ": player2";
-        }
-        catch
-        {
-        }
-        */
-
-
-
     }
 
     // Update is called once per frame
