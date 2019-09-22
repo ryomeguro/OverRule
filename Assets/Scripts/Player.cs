2ec7b85894ea0c8dd4efd5ea84cd0bd92da49e2f
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-22 09:33:09 +0900

    駒を動かせるように

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 30.0f))
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                BoardController.Instance.selectedPiece = hit.collider.gameObject.GetComponent<Piece>();
+                
+                // Debug.Log(selectedObject.transform.position);
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 30.0f))
+            {
+                BoardController.Instance.MovePiece(hit.collider.gameObject.transform.position,hit.collider.gameObject.transform.eulerAngles);
+                //Debug.Log(hit.collider.gameObject.transform.position);
+            }
+        }
     }
 }
