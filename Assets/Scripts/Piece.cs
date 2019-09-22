d948a08588bfa8dfbc2984825e23b11c139d4659
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-22 18:35:31 +0900

    床選択じゃなくても相手取れる

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -21,8 +21,32 @@ public class Piece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.Instance.CurrentPlayerID != playerID)
+        {
+            //piece.playerID.
+            //gameobject.layer=ignore;
+            this.gameObject.layer = 2;
+        }
+
+        if (GameManager.Instance.CurrentPlayerID == playerID)
+        {
+            if (playerID == 0) 
+            {
+                this.gameObject.layer = 8;
+            }
+            else
+            {
+                this.gameObject.layer = 9;
+            }
+
+        }
+
+        //piece1=8,2=9
+
     }
+
+
+
 }
 
 //
