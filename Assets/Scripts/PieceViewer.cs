9457ec450ff59877c30ec48b13acea1bcecc4b5a
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-23 16:36:00 +0900

    アニメ大体

--- Assets/Scripts/PieceViewer.cs
+++ Assets/Scripts/PieceViewer.cs
@@ -17,13 +17,44 @@ public class PieceViewer : MonoBehaviour
         
     }
 
+    private Vector3 movevector;
+    private Vector3 targetposition;
+    private Vector3 direction;
+
     public void Move(Transform plate)
     {
-        transform.position = plate.position;
-        transform.rotation = plate.rotation;
 
-       GameManager.Instance.PlayerChange();
+        targetposition = plate.transform.position;
+        direction = targetposition - transform.position;
+        movevector = (targetposition - transform.position)/20;
+        //transform.forward=direction - transform.forward;
+        //transform.forward = movevector;
+
+        StartCoroutine("Sample");
+        
+        iTween.RotateTo(this.gameObject,iTween.Hash(
+            "x",plate.transform.eulerAngles.x,
+            "y", plate.transform.eulerAngles.y,
+            "z", plate.transform.eulerAngles.z,
+            "time",2.0f)
+            );
+        
+            
+        //transform.Rotate(90, 0, 0);
+        //Debug.Log(this.transform.forward);
+        Debug.Log(direction);
+        GameManager.Instance.PlayerChange();
       
+    }
+
+    private IEnumerator Sample()
+    {
+
+        while (transform.position != targetposition)
+        {
+            transform.position += movevector;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 
     public void Drop(Transform plate)
