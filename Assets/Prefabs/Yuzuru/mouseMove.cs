cf60e8b6d35e29f3de12c8e6231938ffa5bb5384
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-23 09:27:46 +0900

    アニメーションてすと

---
+++ Assets/Prefabs/Yuzuru/mouseMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseMove : MonoBehaviour
+{
+    private RaycastHit hit;
+    private Ray ray;
+
+    private Vector3 targetposition;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                targetposition = hit.point;
+                
+
+            }
+        }
+        if (transform.position != moveposition)
+        {
+            transform.position += movedistance;
+        }
+    }
+}
