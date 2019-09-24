3fd7e8f975737dab08b3fcd366356b37be05ef09
unBabo <42173201+unBabo@users.noreply.github.com>
2019-09-24 09:23:01 +0900

    かめら

--- Assets/Scripts/BoxRotate.cs
+++ Assets/Scripts/BoxRotate.cs
@@ -4,11 +4,109 @@ using UnityEngine;
 
 public class BoxRotate : MonoBehaviour
 {
+
+    private int cameraMode;
+
+    private void Start()
+    {
+        cameraMode = 0;
+        //openingカメラ初期位置player1
+        GameObject.Find("CameraRig").transform.position = new Vector3(0, 0, 0);
+        GameObject.Find("CameraRig").transform.Rotate(0, 0, 0);
+
+        //openingカメラ初期位置player2
+        GameObject.Find("CameraRig").transform.position = new Vector3(0, 0, 0);
+        GameObject.Find("CameraRig").transform.Rotate(0, 0, 0);
+
+    }
+    
+    //コルーチンカメラ制御player1
+    IEnumerator OpeningCamera1()
+    {
+        //openingのカメラ制御
+        iTween.MoveTo(GameObject.Find("CameraRig"), iTween.Hash(
+
+            ));
+        //3秒停止
+        yield return new WaitForSeconds(3);
+        cameraMode = 2;
+    }
+
+    //コルーチンカメラ制御player2
+    IEnumerator OpeningCamera2()
+    {
+        //openingのカメラ制御
+        iTween.MoveTo(GameObject.Find("CameraRig"), iTween.Hash(
+
+            ));
+        //3秒停止
+        yield return new WaitForSeconds(3);
+        cameraMode = 2;
+    }
+
+
+
     void Update()
     {
+        //opening用の3秒たったらカメラ制御可能に
+        if (cameraMode == 0)
+        {
+            StartCoroutine("OpeningCamera1");
+        }else if (cameraMode == 2)
+        {
+            StartCoroutine("OpeningCamera2");
+        }
+        else
+        {
+            //gamescene
+
+        }
+
+        
         if (Input.GetMouseButton(1))
         {
             OnRotate(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        }
+        //マウスホイールクリックしたらデフォポジへ
+        if (Input.GetMouseButtonDown(2))
+        {
+            iTween.RotateTo(GameObject.Find("CameraRig"), iTween.Hash(
+                "rotation",new Vector3(0f,0f,0f),
+                "time",1
+                ));
+
+        }
+        //矢印で上下左右90度回転
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            iTween.RotateAdd(GameObject.Find("CameraRig"), iTween.Hash(
+                "y",90f,
+                "time",1f
+                ));
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            iTween.RotateAdd(GameObject.Find("CameraRig"), iTween.Hash(
+            "y", -90f,
+            "time", 1f
+            ));
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            iTween.RotateAdd(GameObject.Find("CameraRig"), iTween.Hash(
+            "x", 90f,
+            "time", 1f
+            ));
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            iTween.RotateAdd(GameObject.Find("CameraRig"), iTween.Hash(
+            "x", -90f,
+            "time", 1f
+            ));
         }
     }
 
